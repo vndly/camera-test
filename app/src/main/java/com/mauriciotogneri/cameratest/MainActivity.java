ef1650be1f4daafdf246d84ae3a6a2eb7e0d6f93
mtogneri <mtogneri@openwt.com>
2017-05-27 13:01:02 +0200

    Added camera preview package for testing

--- app/src/main/java/com/mauriciotogneri/cameratest/MainActivity.java
+++ app/src/main/java/com/mauriciotogneri/cameratest/MainActivity.java
@@ -1,15 +1,45 @@
 package com.mauriciotogneri.cameratest;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity
+import com.mauriciotogneri.camerapreview.CameraPreview;
+import com.mauriciotogneri.camerapreview.CameraPreviewView;
+
+// https://github.com/pikanji/CameraPreviewSample
+public class MainActivity extends Activity
 {
+    private CameraPreview preview;
+    private CameraPreviewView view;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
+    public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        
+
         setContentView(R.layout.activity_main);
+
+        view = (CameraPreviewView) findViewById(R.id.camerapreview);
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+
+        preview = new CameraPreview(this, 0, CameraPreview.LayoutMode.FitToParent);
+        view.addView(preview);
+
+        preview.setPreviewCallback(view);
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+
+        preview.stop();
+        view.removeView(preview);
+        preview = null;
     }
 }
